050dab3fb2e188350da140c9f4d8b9f283cc1bc8
Kelvin Lau <kelvinlau@Kelvins-MacBook-Pro-3.local>
2017-06-24 10:47:41 -0700

    More progress.

--- FaceMatch/GameViewController.swift
+++ FaceMatch/GameViewController.swift
@@ -7,11 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 final class GameViewController: UIViewController {
+  @IBOutlet fileprivate var previewView: PreviewView!
+  
+  fileprivate let session: AVCaptureSession = {
+    let session = AVCaptureSession()
+    session.sessionPreset = AVCaptureSessionPresetPhoto
+    return session
+  }()
+  
+  fileprivate let output = AVCapturePhotoOutput()
   
   class func instantiate() -> GameViewController {
     let storyboard = UIStoryboard(name: "\(GameViewController.self)", bundle: nil)
     return storyboard.instantiateInitialViewController() as! GameViewController
+  }
+}
+
+// MARK: - Life Cycle
+extension GameViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupCamera()
+  }
+  
+  private func setupCamera() {
+    
+    let backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+    guard let input = try? AVCaptureDeviceInput(device: backCamera) else { fatalError("back camera not available.") }
+    session.addInput(input)
+    session.addOutput(output)
+    previewView.session = session
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    session.startRunning()
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    session.stopRunning()
   }
 }
---
+++ FaceMatch/PreviewView.swift
@@ -0,0 +1,29 @@
+//
+//  PreviewView.swift
+//  FaceMatch
+//
+//  Created by Kelvin Lau on 2017-06-24.
+//  Copyright © 2017 Kelvin Lau. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+final class PreviewView: UIView {
+  override class var layerClass: AnyClass {
+    return AVCaptureVideoPreviewLayer.self
+  }
+  
+  var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+    return layer as! AVCaptureVideoPreviewLayer
+  }
+  
+  var session: AVCaptureSession? {
+    get {
+      return videoPreviewLayer.session
+    }
+    set {
+      videoPreviewLayer.session = newValue
+    }
+  }
+}
